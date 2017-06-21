c9ed64970118b332354f2a4e87211694a8ae45fc
BURIN TECHAMA <benmore1992@gmail.com>
2017-06-21 17:36:38 +0700

    Initial Commit

---
+++ DynamicImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicImage
+//
+//  Created by BURIN TECHAMA on 6/21/2560 BE.
+//  Copyright © 2560 Zeal Tech International. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DynamicImageTests/DynamicImageTests.swift
@@ -0,0 +1,36 @@
+//
+//  DynamicImageTests.swift
+//  DynamicImageTests
+//
+//  Created by BURIN TECHAMA on 6/21/2560 BE.
+//  Copyright © 2560 Zeal Tech International. All rights reserved.
+//
+
+import XCTest
+@testable import DynamicImage
+
+class DynamicImageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
